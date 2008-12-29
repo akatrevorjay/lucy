5d5bf479d6b166eabb190ff361cfb37b382a765d
Trevor Joynson (trevorj) <github@skywww.net>
2008-12-29 08:22:45 +0000

    Hopefully filter out crap that alicesocket can't take
    git-svn-id: https://svn.skywww.net/repos/skywww/lucy/trunk@79 18765250-b91b-0410-8a00-f77a687b104c

--- Lucy/Diamonds/Alice.pm
+++ Lucy/Diamonds/Alice.pm
@@ -80,6 +80,10 @@ sub do_alice {
 
 	my $out;
 	if ( my $sock = IO::Socket::UNIX->new('/tmp/alice') ) {
+		# remove any odd characters in $msg, because alicesocket does a
+		#   terrible job at staying alive when they are handed to her.
+		$msg =~ s/[^0-9a-z\!\@\#\$\%\^\&\*\(\)\-_\=\+[]\<\>\,\.\/\\?]//gi;
+		
 		$sock->write("$who\007$msg");
 		$sock->read( $out, 1024 );
 		$sock->close;
